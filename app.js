1df0b070e7a2b209350a1e2d89d0813b86fcbf6b
Mahir Tazwar <mahirtaz@gmail.com>
2022-05-03 14:39:32 +0600

    fetch api called to generate random persons

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const app = Vue.createApp({
       lastName: 'Doe',
       email: 'john.doe@gmail.com',
       gender: 'male',
-      picture: 'https://i.pinimg.com/236x/8a/f2/a0/8af2a0c328704957a1375be1e8ff7c87---years-evan.jpg',
+      picture: "https://randomuser.me/api/portraits/men/18.jpg",
     }
-  }
+  },
+  methods: {
+    async getUser(){
+      const res = await fetch('https://randomuser.me/api')
+     
+      const {results} = await res.json()
+      console.log(results)
+      this.firstName = results[0].name.first
+      this.lastName= results[0].name.last
+      this.email= results[0].email
+      this.gender= results[0].gender
+      this.picture= results[0].picture.large
+    },
+  },
 })
 
 app.mount('#app') 
